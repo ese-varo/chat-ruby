d26b459b6c53c3455c6a6a615fbbeeb9ec06f6f6
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-21 16:19:09 -0600

    Add image to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
-  before_action :message, only: %i!edit update destroy!
+  # before_action :message, except: %i!edit update destroy!
+  before_action :message, except: :create
   before_action :conversation
 
   def create
@@ -17,18 +18,24 @@ class MessagesController < ApplicationController
 
   def destroy
     @message.remove
+    @message.image.purge
     @message.save
     redirect_to @conversation
+  end
+
+  def remove_asset
+    @message.image.purge
+    redirect_to edit_conversation_message_path @message
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:content, :conversation_id)
+    params.require(:message).permit(:content, :conversation_id, :image)
   end
 
   def new_message_params
-    params.permit(:content, :conversation_id)
+    params.permit(:content, :conversation_id, :image)
   end
 
   def message
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
+  
+  has_one_attached :image
 
   def remove
     self.removed = true

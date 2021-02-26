ba289d6f6d7822eb6aac85629a1a74a70cb9bf38
Alvaro Padilla <varoboss.ap@gmail.com>
2021-02-25 19:05:09 -0600

    Add form partial for conversations

--- app/jobs/send_conversations_summary_email_job.rb
+++ app/jobs/send_conversations_summary_email_job.rb
@@ -11,21 +11,17 @@ class SendConversationsSummaryEmailJob < ApplicationJob
 
   private
   def summary
-    { conversations: new_conversations.count, messages: messages }
+    { conversations: new_conversations.count, messages: new_messages_count }
   end
 
   def new_conversations
-    @new_conversations ||= @user.conversation.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @new_conversations ||= @user.conversations.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 
   def new_messages_count
-    today = Date.today
-    messages = 0
-    @user.conversations.each do |c|
-      c.messages.reverse.each { |m| m.created_at.to_date < today ? break : messages += 1 }
-      # messages += c.messages.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @user.conversations.reduce(0) do |sum, c|
+      sum += c.messages.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
     end
-    messages
   end
 
   def reschedule_job

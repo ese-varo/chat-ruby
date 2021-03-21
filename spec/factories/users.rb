487a2439d10c1319afe56d460c145a3edcaf2a3a
Alvaro Padilla <varoboss.ap@gmail.com>
2021-03-21 13:10:58 -0600

    Use faker generator for user passwords

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.username(specifier: 4..9) }
     email { Faker::Internet.email }
-    password { 'lapassword' }
-    password_confirmation { 'lapassword' }
+    password { Faker::Internet.password(min_length: 8) }
+    password_confirmation { password }
   end
 end

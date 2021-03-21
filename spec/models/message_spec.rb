a1e40dc3bbeff69939adc5e364b5cfbc30b72fb8
Alvaro Padilla <varoboss.ap@gmail.com>
2021-03-21 14:23:10 -0600

    Refactor message spec model using factories

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,25 +1,14 @@
 require 'rails_helper'
 
-describe Message do
-  before :each do
-    @user = User.create(
-      username: 'zergio',
-      email: 'zergio@gmail.com',
-      password: 'elzergio',
-      password_confirmation: 'elzergio')
-    @conversation = Conversation.create(
-      name: 'The conversation',
-      description: 'Lets talk about rails apps',
-      emoji: ':dog_face:')
-    @message = @conversation.messages.build(
-      content: "Hello world!",
-      user_id: @user.id)
-  end
+RSpec.describe Message do
+  let(:user) { create(:user) }
+  let(:conversation) { create(:conversation, messages: [message]) }
+  let(:message) { create(:message, user: user) }
   describe "A valid Message" do
     it "has any text content" do
-      message = @conversation.messages.build(
+      message = conversation.messages.build(
         content: "Hello world!",
-        user_id: @user.id)
+        user_id: user.id)
       expect(message).to be_valid
       message.update_attribute :content, "@#$%!@#^& 123hs rand ///hsl"
       expect(message.errors[:content]).to be_empty
@@ -33,16 +22,16 @@ describe Message do
 
   describe "An invalid Message" do
     it "has no text content" do
-      message = @conversation.messages.build(user_id: @user.id)
+      message = conversation.messages.build(user_id: user.id)
       message.valid?
       expect(message.errors[:content]).to include("can't be blank")
     end
   end
 
   it "lets me change its text content" do
-    previous_content = @message.content
-    @message.update_attribute :content, "I'm already different"
-    expect(@message.content).not_to eq previous_content
+    previous_content = message.content
+    message.update_attribute :content, "I'm already different"
+    expect(message.content).not_to eq previous_content
   end
 
   it "lets me change its attached image" do
@@ -51,7 +40,7 @@ describe Message do
   end
 
   it "returns a date string" do
-    @message.save
-    expect(@message.date).to be_a_kind_of String
+    message.save
+    expect(message.date).to be_a_kind_of String
   end
 end

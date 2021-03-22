c49f537a9ff6628b0a0d9dd693799e6fefad2949
Alvaro Padilla <varoboss.ap@gmail.com>
2021-03-22 01:00:57 -0600

    Add feature to show the profile of a public conversation

--- spec/system/public_conversations_spec.rb
+++ spec/system/public_conversations_spec.rb
@@ -1,14 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "visit index without log in" do
-  it "display the most active public conversations" do
+  before(:each) do
     create(:conversation, messages: [create(:message)])
     create(:conversation, messages: [create(:message)])
     create(:conversation, messages: [create(:message)])
     create(:conversation, messages: [create(:message)])
     create(:conversation, messages: [create(:message)])
     create(:conversation, name: 'Rails conversation', messages: [create(:message)])
+  end
 
+  it "display the most active public conversations" do
     visit(root_path)
     expect(page).to have_selector("h1", text: "Welcome!")
     within(".card-body") do
@@ -17,5 +19,14 @@ RSpec.describe "visit index without log in" do
     end
     expect(page).to have_selector("a", text: "Profile", count: 5)
     expect(page).to have_selector("h4", text: /Rails conversation/)
+  end
+
+  it "display the profile of a conversation" do
+    another_conversation = create(:conversation,
+      name: 'Tell me more!', messages: [create(:message)])
+    visit(root_path)
+    find("#conversation_#{another_conversation.id} a").click
+    expect(page).to have_selector(
+      "h3", text: another_conversation.name)
   end
 end

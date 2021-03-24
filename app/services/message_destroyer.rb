3ec5bb6c52b454074f0103511ba9b2ab468c4644
Alvaro Padilla <varoboss.ap@gmail.com>
2021-03-23 19:54:03 -0600

    Refactor message destroyer

--- app/services/message_destroyer.rb
+++ app/services/message_destroyer.rb
@@ -5,7 +5,6 @@ class MessageDestroyer < ApplicationService
 
   def call
     @message.removed = true
-    @message.content = 'deleted'
     @message.image.purge
     if @message.save
       OpenStruct.new({success?: true})
